94cf49d754bc692df7b7c72017d09c1c94a6fddc
vashukhanpara <125720009+vashukhanpara@users.noreply.github.com>
2023-04-20 09:48:29 +0530

    stack and queue data
    This is program in which you can perform push and pop operation. It take user input for perform operation which you want. there is two part of program, first part show how to data add and delete in list and second part show that how to data add and delete in queue When you will be out of moves the program will be exit

--- datatype stack and queue.py
+++ datatype stack and queue.py
@@ -47,62 +47,62 @@ Press 5 for Exit
     f += 1
 else:
     print('    sorry , out of moves')
-# pa6i apde ama new ghanu add kari sakie jemk length of stack avi rite ghanu 
 
 
 
 
-# queue wadu works in lilo or fifo
-# q = []
-# f= 0
-# while f < 10 :
-#     user_input = int(input('''
-# press 1 for Push element
-# Press 2 for pop element
-# Press 3 for first element
-# Press 4 for last element
-# Press 5 for Display complete queue
-# Press 6 for Exit
 
-# ---->'''))
-#     if user_input == 1:
-#         n = input('    Enter the push value:')
-#         q.append(n)
-#         print('    Your new queue is' , q)
+# queue works in lilo or fifo
+q = []
+f= 0
+while f < 10 :
+    user_input = int(input('''
+press 1 for Push element
+Press 2 for pop element
+Press 3 for first element
+Press 4 for last element
+Press 5 for Display complete queue
+Press 6 for Exit
 
-#     elif user_input == 2:
-#         if len(q)==0:
-#             print('    Empty queue')
-#         else:
-#             first_element = q[0] 
-#             del q[0]#jyare first program ma apde ahiya del ni jagyae pop no use karta mate e delete pa6adthi j thatu tu
-#             print('    Your deleted queue is' ,first_element)
-#             print ('    Your new queue is' ,q)
+---->'''))
+    if user_input == 1:
+        n = input('    Enter the push value:')
+        q.append(n)
+        print('    Your new queue is' , q)
+
+    elif user_input == 2:
+        if len(q)==0:
+            print('    Empty queue')
+        else:
+            first_element = q[0] 
+            del q[0]
+            print('    Your deleted queue is' ,first_element)
+            print ('    Your new queue is' ,q)
      
-#     elif user_input == 3:
-#         if len(q)==0:
-#             print('    Empty queue')
-#         else:
-#             print('    First  queue Element is ' , q[0])
+    elif user_input == 3:
+        if len(q)==0:
+            print('    Empty queue')
+        else:
+            print('    First  queue Element is ' , q[0])
 
-#     elif user_input==4:
-#         if len(q)==0:
-#             print('    Empty queue')
-#         else:
-#             print("    Last  queue Element is",q[-1])
+    elif user_input==4:
+        if len(q)==0:
+            print('    Empty queue')
+        else:
+            print("    Last  queue Element is",q[-1])
 
-#     elif user_input==5:
-#         if len(q)==0:
-#             print('    Empty queue')
-#         else:
-#             print('Full Queue is' , q)
+    elif user_input==5:
+        if len(q)==0:
+            print('    Empty queue')
+        else:
+            print('Full Queue is' , q)
         
-#     elif user_input ==6:
-#         break
+    elif user_input ==6:
+        break
 
-#     else:
-#         print('    Sorry , invalid input')
+    else:
+        print('    Sorry , invalid input')
          
-#     f += 1
-# else:
-#     print('--------sorry , out of moves----------------------------')
+    f += 1
+else:
+    print('--------sorry , out of moves----------------------------')
