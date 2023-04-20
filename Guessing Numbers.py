0ea0bef6d3d8f8e6313a43e4a42b8d44e8d0800f
vashukhanpara <125720009+vashukhanpara@users.noreply.github.com>
2023-04-20 20:07:43 +0530

    Guessing number
    This is small project related to guessing numbers in which user will write any number and program will show user  that guessing number is  small or big. In this user can try only 9 time.

--- Guessing Numbers.py
+++ Guessing Numbers.py
@@ -1,23 +1,3 @@
-# num1 = int(input("press 1 and enter for start quize :"))
-
-# while num1<22:
-#     num1 = int(input("please enter your number  :"))
-#     print("you enter small number please try again")
-# else:
-#     pass
-
-# while num1>25:
-#     num1 = int(input("please enter your number :"))
-#     print("you entered big number please try again")
-# else:
-#     print("congratulation, you won")
-
-
-# that was my logik which is not right
-# now we will write real program for this
-
-
-
 n=18
 number_of_guesses=1
 print("number of guesses is limited to only 9 times")
@@ -30,7 +10,7 @@ while (number_of_guesses<=9):
     else:
         print("you won")
         print(number_of_guesses,"took to finish")
-        break#here we can also use of break
+        break
     print(9-number_of_guesses,"no.of guesses left")
     number_of_guesses = number_of_guesses+1
 
